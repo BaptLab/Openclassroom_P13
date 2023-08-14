9fa6663e3ee7af8a17d056b948e49bf0a21e9792
Baptiste LABAUNE <baptiste.labaune@gmail.com>
2023-08-14 17:19:34 +0200

    axios login working

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import Routing from "./Routing";
 import { Provider } from "react-redux";
 import store from "./redux/store/store"; // Import the default export
+import { AuthProvider } from "./context/AuthProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Routing />
-    </Provider>
+    <AuthProvider>
+      <Provider store={store}>
+        <Routing />
+      </Provider>
+    </AuthProvider>
   </React.StrictMode>
 );
--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -2,8 +2,58 @@ import Button from "../components/Button/Button";
 import RedirectButton from "../components/Button/RedirectButton";
 import Footer from "../components/layout/Footer";
 import Header from "../components/layout/Header";
+import { useRef, useState, useEffect, useContext } from "react";
+import AuthContext from "../context/AuthProvider";
+import axios from "axios";
+const Login_URL = "http://localhost:3001/api/v1/user/login";
 
 function Login() {
+  const { setAuth } = useContext(AuthContext);
+
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errMesg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd]);
+
+  const HandleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(Login_URL, JSON.stringify({ email: user, password: pwd }));
+    try {
+      const response = await axios.post(
+        Login_URL,
+        JSON.stringify({ email: user, password: pwd }),
+        { headers: { "Content-Type": "application/json" }, withCredentials: true }
+      );
+      console.log(JSON.stringify(response?.data));
+      const accessToken = response?.data?.token;
+      setAuth({ user, pwd, accessToken });
+      setUser("");
+      setPwd("");
+      setSuccess("");
+    } catch (err) {
+      if (!err?.response) {
+        setErrMsg("No server Response");
+      } else if (err.response?.status === 400) {
+        setErrMsg("Invalid Fields");
+      } else if (err.response?.status === 500) {
+        setErrMsg("internal server error");
+      } else {
+        setErrMsg("Login Failed");
+      }
+    }
+  };
+
   return (
     <div className="page-container">
       <Header />
@@ -11,22 +61,40 @@ function Login() {
         <section className="sign-in-content">
           <i className="fa fa-user-circle sign-in-icon"></i>
           <h1>Sign In</h1>
-          <form id="sign-in-box">
+          <form id="sign-in-box" onSubmit={HandleSubmit}>
             <div className="input-wrapper">
-              <label for="username">Username</label>
-              <input type="text" id="username" />
+              <label>Username</label>
+              <input
+                type="text"
+                id="username"
+                autoComplete="off"
+                ref={userRef}
+                onChange={(e) => {
+                  setUser(e.target.value);
+                }}
+                value={user}
+                required
+              />
             </div>
             <div className="input-wrapper">
-              <label for="password">Password</label>
-              <input type="password" id="password" />
+              <label>Password</label>
+              <input
+                type="password"
+                id="password"
+                onChange={(e) => setPwd(e.target.value)}
+                value={pwd}
+                required
+              />
             </div>
             <div className="input-remember">
               <input type="checkbox" id="remember-me" />
-              <label for="remember-me">Remember me</label>
+              <label>Remember me</label>
             </div>
-            <Button>
+            <button className="transaction-button">Sign in</button>
+            {/* <Button>
+              Sign in
               <RedirectButton action="Sign in" url="/user" />
-            </Button>
+            </Button> */}
           </form>
         </section>
       </main>
